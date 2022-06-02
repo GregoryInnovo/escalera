6825b1eeea8b2a69596dd04e663f2683bcf7b8be
Gregson Murcia Castro <gregoryinnovo@gmail.com>
2022-06-02 19:00:49 +0000

    [FIX] move function logic

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let currentPosition = 0; // start the current position of the player
 let board = [
   0, 0, 11, 0, 0, 17, 0, 0, 18, 12, 0, 0, 0, 4, 0, 0, 0, 0, 8, 0, 0, 20, 0, 16,
-  0,
+  0, 0, 0, 0, 0, 0, 0,
 ]; // create an array that contains the new positions, but depends of current position of the player
 
 /**
@@ -32,18 +32,25 @@ function move(steps) {
   let definePosition = currentPosition + steps;
   let boardValue = board[definePosition - 1];
 
+  // In this conditional the game examine if the board value is different than 0
   if (boardValue === 0) {
+    // If the conditional is true the current position advances according to the value of the dice
     currentPosition += steps;
-    currentPosition != null;
-    console.log("Jugador avanza a cuadro:", currentPosition);
+    if (currentPosition > 25) {
+      console.log("Jugador supera el cuadro 25");
+    } else {
+      console.log("Jugador avanza al cuadro:", currentPosition);
+    }
   } else {
+    // Update the current position
     currentPosition = boardValue;
+    // In this conditional the program examine if the player goes forward or backwards
     if (definePosition < currentPosition) {
       console.log("Jugador sube por escalera al cuadro", currentPosition);
     } else {
-      boardValue !== 0 && currentPosition !== undefined
-        ? console.log("Jugador desciende al cuadro", currentPosition)
-        : console.log("Jugador supera el cuadro 25");
+      boardValue !== 0 &&
+        currentPosition !== undefined &&
+        console.log("Jugador desciende al cuadro", currentPosition);
     }
   }
 }
