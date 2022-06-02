dd53d3095f5962adaf8d1079461b7fbd761e0dfd
Gregson Murcia Castro <gregoryinnovo@gmail.com>
2022-06-02 17:43:21 +0000

    [ADD] JSDoc and fix bug

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-let currentPosition = 0;
+let currentPosition = 0; // start the current position of the player
 let board = [
   0, 0, 11, 0, 0, 17, 0, 0, 18, 12, 0, 0, 0, 4, 0, 0, 0, 0, 8, 0, 0, 20, 0, 16,
   0,
-];
+]; // create an array that contains the new positions, but depends of current position of the player
 
-// TODO: doc game function
+/**
+ * Run the game
+ */
 function game() {
   while (currentPosition < 25) {
     let diceNum = getRandomNumber();
@@ -14,16 +16,21 @@ function game() {
   console.log("Fin");
 }
 
-// TODO: Doc getRandomNumber function
+/**
+ * Get a random number
+ * @returns {number} a random number
+ */
 const getRandomNumber = () => {
   return parseInt(Math.random() * (7 - 1) + 1);
 };
 
-// TODO: Doc move function
+/**
+ * Move the player in the board depends of the board values and dice number
+ * @param {number} steps that the player can move
+ */
 function move(steps) {
   let definePosition = currentPosition + steps;
   let boardValue = board[definePosition - 1];
-  let oldPos = currentPosition;
 
   if (boardValue === 0) {
     currentPosition += steps;
@@ -31,12 +38,14 @@ function move(steps) {
     console.log("Jugador avanza a cuadro:", currentPosition);
   } else {
     currentPosition = boardValue;
-    currentPosition > oldPos
-      ? console.log("Jugador sube por escalera al cuadro", currentPosition)
-      : boardValue !== 0 && currentPosition !== undefined
-      ? console.log("Jugador desciende al cuadro", currentPosition)
-      : console.log("Jugador supera el cuadro 25");
+    if (definePosition < currentPosition) {
+      console.log("Jugador sube por escalera al cuadro", currentPosition);
+    } else {
+      boardValue !== 0 && currentPosition !== undefined
+        ? console.log("Jugador desciende al cuadro", currentPosition)
+        : console.log("Jugador supera el cuadro 25");
+    }
   }
 }
 
-game();
+game(); // execute the game
