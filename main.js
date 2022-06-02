1dbdf680cd6453425062070743aab0b76fe9c32e
Gregson Murcia Castro <gregoryinnovo@gmail.com>
2022-06-02 16:28:51 +0000

    [UPDATE] Finish game logic

--- main.js
+++ main.js
@@ -4,32 +4,39 @@ let board = [
   0,
 ];
 
-// TODO: Add restrictions
+// TODO: doc game function
 function game() {
   while (currentPosition < 25) {
     let diceNum = getRandomNumber();
     console.log("Dado arroja:", diceNum);
     move(diceNum);
   }
+  console.log("Fin");
 }
 
-// TODO: Improve function
+// TODO: Doc getRandomNumber function
 const getRandomNumber = () => {
   return parseInt(Math.random() * (7 - 1) + 1);
 };
 
-// TODO: improve steps value depends the position
+// TODO: Doc move function
 function move(steps) {
   let definePosition = currentPosition + steps;
   let boardValue = board[definePosition - 1];
+  let oldPos = currentPosition;
 
-  boardValue === 0
-    ? (currentPosition += steps)
-    : (currentPosition = board[definePosition - 1]);
-
-  currentPosition != null
-    ? console.log("La posicion de jugador es:", currentPosition)
-    : console.log("El jugador supera el cuadro 25");
+  if (boardValue === 0) {
+    currentPosition += steps;
+    currentPosition != null;
+    console.log("Jugador avanza a cuadro:", currentPosition);
+  } else {
+    currentPosition = boardValue;
+    currentPosition > oldPos
+      ? console.log("Jugador sube por escalera al cuadro", currentPosition)
+      : boardValue !== 0 && currentPosition !== undefined
+      ? console.log("Jugador desciende al cuadro", currentPosition)
+      : console.log("Jugador supera el cuadro 25");
+  }
 }
 
 game();
